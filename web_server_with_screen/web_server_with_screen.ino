6d3d40e3233726bbf21529caadc308c9a9d8b76f
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-26 10:53:37 +0300

    Update web_server_with_screen.ino

--- web_server_with_screen/web_server_with_screen.ino
+++ web_server_with_screen/web_server_with_screen.ino
@@ -41,22 +41,6 @@ const char index_html[] PROGMEM = R"rawliteral(
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <style>
-    html {
-     font-family: Arial;
-     display: inline-block;
-     margin: 0px auto;
-     text-align: left;
-    }
-    h2 { font-size: 3.0rem; }
-    p { font-size: 3.0rem; }
-    .units { font-size: 1.2rem; }
-    .dht-labels{
-      font-size: 1.5rem;
-      vertical-align:middle;
-      padding-bottom: 5px;
-    }
-  </style>
 </head>
 <body>
   <p>
